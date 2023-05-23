5a21af3a31cde8f356487f1b0c0455629249d159
pparkjs <qkrwjdtn741@naver.com>
2023-05-23 17:35:24 +0900

    DateTimeFormat 어노테이션 추가

--- DevProject/src/main/java/kr/or/ddit/vo/Member.java
+++ DevProject/src/main/java/kr/or/ddit/vo/Member.java
@@ -1,9 +1,17 @@
 package kr.or.ddit.vo;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class Member {
+	private String userId = "a001";
 	private String userName = "hongkd";
 	private String password = "1234";
+	private int coin = 100;
+	@DateTimeFormat(pattern = "yyyyMMdd")
+	private Date dateOfBirth;
 }
