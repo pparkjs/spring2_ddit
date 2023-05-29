1e1e42fbe36d3c9a21d70a7bdb4aec1ff6ebabfb
pparkjs <qkrwjdtn741@naver.com>
2023-05-30 08:55:47 +0900

    수업 들은 자료 ( 중요하지 않았던 내용 추가 )

--- DevProject/src/main/java/kr/or/ddit/vo/Member.java
+++ DevProject/src/main/java/kr/or/ddit/vo/Member.java
@@ -15,6 +15,20 @@ public class Member {
 	private int coin = 100;
 	@DateTimeFormat(pattern = "yyyyMMdd")
 	private Date dateOfBirth;
+	private String email;
+	private String gender;
+	private String hobby;
+	private String[] hobbyArray;
+	private List<String> hobbyList;
+	private boolean foreigner;
+	private String developer;
+	private String nationality;
 	private Address address;
 	private List<Card> cardList;
+	private String cars;
+	private String[] carArray;
+	private List<String> carList;
+	
+	private String introduction;
+	private String birthDay;
 }
