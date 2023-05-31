ecfb51c3d994b2d2bea7d2e062fe48154a661696
pparkjs <qkrwjdtn741@naver.com>
2023-05-31 17:25:55 +0900

    입력값 겁증 규칙 설정위한 어노테이션 추가

--- DevProject/src/main/java/kr/or/ddit/vo/Member.java
+++ DevProject/src/main/java/kr/or/ddit/vo/Member.java
@@ -3,18 +3,30 @@ package kr.or.ddit.vo;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
 @Data
 public class Member {
+	@NotBlank(message = "야 ID가 비었잖아 ㅡ,.ㅡ")
 	private String userId = "a001";
+	@NotBlank(message = "야 이름이 비었잖아 ㅡ,.ㅡ")
+	@Size(max = 3)
 	private String userName = "hongkd";
+	@NotBlank(message = "야 비밀번호가 비었잖아 ㅡ,.ㅡ")
 	private String password = "1234";
 	private int coin = 100;
-	@DateTimeFormat(pattern = "yyyyMMdd")
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dateOfBirth;
+	@Email
 	private String email;
 	private String gender;
 	private String hobby;
@@ -23,7 +35,11 @@ public class Member {
 	private boolean foreigner;
 	private String developer;
 	private String nationality;
+	// 중첩된 자바빈즈의 입력값 검증을 지정한다.
+	@Valid
 	private Address address;
+	// 중첩된 자바빈즈의 입력값 검증을 지정한다.
+	@Valid
 	private List<Card> cardList;
 	private String cars;
 	private String[] carArray;
