ca545b7416dbce3987ab23d4726e4d38dc8e0cb4
pparkjs <qkrwjdtn741@naver.com>
2023-06-02 08:59:22 +0900

    게시판 service interface 추가

---
+++ DevProject/src/main/java/kr/or/ddit/service/IBoardService.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.service;
+
+import java.util.List;
+
+import kr.or.ddit.vo.Board;
+
+public interface IBoardService {
+
+	public void register(Board board);
+
+	public List<Board> list();
+
+	public Board read(int boardNo);
+
+	public int update(Board board);
+
+	public void delete(int boardNo);
+
+	public List<Board> search(Board board);
+}
