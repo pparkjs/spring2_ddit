6fb5040cdce0581345846faf4e57381db8eabb5b
pparkjs <qkrwjdtn741@naver.com>
2023-06-05 17:41:48 +0900

    단일 파일 업로드 위한 Service, controller 추가

---
+++ DevProject/src/main/java/kr/or/ddit/controller/file/item01/FileUploadController01.java
@@ -0,0 +1,230 @@
+package kr.or.ddit.controller.file.item01;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import kr.or.ddit.controller.file.item01.service.ItemService;
+import kr.or.ddit.vo.Item;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/item")
+@Slf4j
+public class FileUploadController01 {
+/*
+ * 13장 파일업로드
+ * 
+ * 1. 파일업로드 설명
+ * 
+ * 	- 파일 업로드 등록 기능 컨트롤러 메소드 만들기 (itemRegister:post)
+ * 	- 파일 업로드 등록 기능 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 등록 기능 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 등록 기능 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 등록 기능 Mapper xml 쿼리 만들기
+ * 	- 파일 업로드 등록 완료 페이지 만들기.
+ *  - 여기까지 확인 
+ *  
+ * 	- 파일 업로드 목록 화면 컨트롤러 메소드 만들기 (itemList:post)
+ * 	- 파일 업로드 목록 화면 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 목록 화면 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 목록 화면 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 목록 화면 Mapper xml 쿼리 만들기
+ * 	- 파일 업로드 목록 화면  만들기(item/list).
+ *  - 여기까지 확인 
+ *  
+ * 	- 파일 업로드 수정 화면 컨트롤러 메소드 만들기 (itemModifyForm:post)
+ * 	- 파일 업로드 수정 화면 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 수정 화면 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 수정 화면 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 수정 화면 Mapper xml 쿼리 만들기
+ * 	- 파일 업로드 수정 화면  만들기(item/modify.jsp).
+ *  - 파일 업로드 수정 기능 컨트롤러 메소드 만들기 (itemModify:post)
+ * 	- 파일 업로드 수정 기능 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 수정 기능 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 수정 기능 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 수정 기능 Mapper xml 쿼리 만들기
+ *  - 여기까지 확인 
+ 
+ * 	- 파일 업로드 삭제 화면 컨트롤러 메소드 만들기 (itemRemoveForm:get)
+ * 	- 파일 업로드 삭제 화면 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 삭제 화면 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 삭제 화면 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 삭제 화면 Mapper xml 쿼리 만들기
+ * 	- 파일 업로드 삭제 화면  만들기 (item/remove.jsp)
+ *  - 파일 업로드 삭제 기능 컨트롤러 메소드 만들기 (itemRemove:post)
+ * 	- 파일 업로드 삭제 기능 서비스 인터페이스 메소드 만들기
+ * 	- 파일 업로드 삭제 기능 서비스 클래스 메소드 만들기
+ * 	- 파일 업로드 삭제 기능 Mapper 인터페이스 메소드 만들기
+ * 	- 파일 업로드 삭제 기능 Mapper xml 쿼리 만들기
+ *  - 여기까지 확인 
+ */
+	
+	// root-context.xml 에서 설정한 uploadPath 빈등록 path 경로를 사용한다.(value로 설정했던 값)
+	@Resource(name="uploadPath")
+	private String resourcesPath;
+//	private String uploadPath = "C://upload";
+	
+	@Inject
+	private ItemService itemService;
+	
+	// 등록 페이지로 가는 메소드
+	@RequestMapping(value="/register", method = RequestMethod.GET)
+	public String itemRegisterForm() {
+		return "item/register";
+	}
+	
+	// 파일 등록을 진행하는 메소드
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String itemRegister(Item item, Model model) throws IOException {
+		MultipartFile file = item.getPicture();
+		
+		log.info("originalName : " + file.getOriginalFilename());
+		log.info("size : " + file.getSize());
+		log.info("contentType : " + file.getContentType());
+		
+		// 넘겨받은 파일을 이용하여 파일 업로드(복사)를 진행한다.
+		// return : UUID + "_" _ 원본 파일명
+		String createdFileName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		item.setPictureUrl(createdFileName);
+		itemService.register(item);
+		model.addAttribute("msg", "등록이 완료되었습니다");
+		return "item/success";
+	}
+	
+	// 업로드할 파일 생성
+	private String uploadFile(String originalName, byte[] fileData) throws IOException {
+		System.out.println("resourcesPath : " + resourcesPath);
+		UUID uuid = UUID.randomUUID(); // UUID 파일명 생성 준비
+		String createdFileName = uuid.toString() + "_" + originalName; //UUID + "_" + 원본파일명
+		
+		File file = new File(resourcesPath);
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		
+		File target = new File(resourcesPath, createdFileName);
+		FileCopyUtils.copy(fileData, target); // 파일 복사
+		return createdFileName;
+	}
+	
+	// 파일 목록 화면 이동 메소드
+	@RequestMapping(value="/list", method = RequestMethod.GET)
+	public String itemList(Model model) {
+		List<Item> itemList = itemService.list();
+		model.addAttribute("itemList", itemList);
+		return "item/list";
+	}
+	
+	// 수정 폼으로 이동
+	@RequestMapping(value="/modify", method = RequestMethod.GET)
+	public String itemModifyForm(int itemId, Model model) {
+		Item item = itemService.read(itemId);
+		model.addAttribute("item", item);
+		return "item/modify";
+	}
+	
+	// 파일 미리보기를 위한 메소드
+	@RequestMapping(value="/display")
+	public ResponseEntity<byte[]> displayFile(int itemId) throws FileNotFoundException{
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		//파일 미리보기를 위해 DB에 저장된 해당 번호에 파일 이름 가져오기
+		String fileName = itemService.getPicture(itemId);
+		log.info("fileName : " + fileName);
+		
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".") + 1); // 확장자 추출
+			MediaType mType = getMediaType(formatName); // 이미지인지 아닌지 판단.
+			HttpHeaders headers = new HttpHeaders();
+			
+			//File.separator -> '/' 이런 애
+			in = new FileInputStream(resourcesPath + File.separator + fileName);
+			if(mType != null) {
+				headers.setContentType(mType);
+			}
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+		} catch (IOException e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			try {
+				in.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return entity;
+	}
+	
+	// 해당 파일의 확장자 뽑아내기
+	public MediaType getMediaType(String formatName) {
+		if(formatName != null) {
+			if(formatName.toUpperCase().equals("JPG")) {
+				return MediaType.IMAGE_JPEG;
+			}
+			if(formatName.toUpperCase().equals("GIF")) {
+				return MediaType.IMAGE_GIF;
+			}
+			if(formatName.toUpperCase().equals("PNG")) {
+				return MediaType.IMAGE_PNG;
+			}
+		}
+		return null;
+	}
+	
+	// 이미지 수정하기 메소드
+	@RequestMapping(value="/modify", method = RequestMethod.POST)
+	public String itemModify(Item item, Model model) throws IOException {
+		MultipartFile file = item.getPicture();
+		
+		if(file != null && file.getSize() > 0) {
+			log.info("originalName : " + file.getOriginalFilename());
+			log.info("size : " + file.getSize());
+			log.info("contentType : " + file.getContentType());
+			
+			String createdFileName = uploadFile(file.getOriginalFilename(), file.getBytes());
+			item.setPictureUrl(createdFileName);
+		}
+		
+		itemService.modify(item);
+		model.addAttribute("msg", "수정이 완료되었습니다");
+		return "item/success";
+	}
+	
+	// 삭제페이지로 이동하는 메소드
+	@RequestMapping(value="/remove", method = RequestMethod.GET)
+	public String itemRemoveForm(int itemId, Model model) {
+		Item item = itemService.read(itemId);
+		model.addAttribute("item", item);
+		return "item/remove";
+	}
+	
+	// 삭제를 진행하는 메소드
+	@RequestMapping(value="/remove", method = RequestMethod.POST)
+	public String itemRemove(int itemId, Model model) {
+		itemService.remove(itemId);
+		model.addAttribute("msg", "삭제가 완료되었습니다");
+		return "item/success";
+	}
+}
