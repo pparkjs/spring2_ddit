403be6b3ae1e168b87b51831240109e27688bc41
pparkjs <qkrwjdtn741@naver.com>
2023-06-05 17:53:06 +0900

    공지사항 mapper interface 추가

--- DevProject/src/main/java/kr/or/ddit/mapper/NoticeMapper.java
+++ DevProject/src/main/java/kr/or/ddit/mapper/NoticeMapper.java
@@ -1,10 +1,16 @@
 package kr.or.ddit.mapper;
 
+import java.util.List;
+
 import kr.or.ddit.vo.NoticeVO;
+import kr.or.ddit.vo.PaginationInfoVO;
 
 public interface NoticeMapper {
 	public int insertNotice(NoticeVO noticeVO);
 	public void incrementHit(int boNo);
 	public NoticeVO selectNotice(int boNo);
 	public int updateNotice(NoticeVO noticeVO);
+	public int deleteNotice(int boNo);
+	public int selectNoticeCount(PaginationInfoVO<NoticeVO> pagingVO);
+	public List<NoticeVO> selectNoticeList(PaginationInfoVO<NoticeVO> pagingVO);
 }
